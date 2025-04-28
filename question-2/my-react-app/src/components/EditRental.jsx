120b63efad892544f96d6ad2b10789e0fed77e29
shatakshisachan051 <shatakshisachan1017@gmail.com>
2025-04-28 13:29:01 +0530

    28 april

--- question-2/my-react-app/src/components/EditRental.jsx
+++ question-2/my-react-app/src/components/EditRental.jsx
@@ -1,16 +1,26 @@
 import React from "react";
+import { useDispatch } from "react-redux";
 import { useParams } from "react-router-dom";
 
 function EditRental() {
   const { id } = useParams();
+  const [customer,setCustomer]= useState('');
+  const [carType,setCarType] = useState('');
+  const [rentalDays, setRentalDays]=useState('');
+  const dispatch =useDispatch();
   return (
     <div>
       <h1>Edit rental {id}</h1>
       <form>
-        <input type="text" name="customer" placeholder="Customer Name..." />
-        <input type="text" name="carType" placeholder="Car Type..." />
-        <input type="number" name="rentalDays" placeholder="renttal days..." />
-        <button type="submit"> Update </button>
+      <form onSubmit={handleSumbit} >
+            <input type="text" name="customer" value={customer} onChange={(e)=> setCustomer(e.target.value) } placeholder='Customer Name...' />
+            <input type="text" name="carType" value={carType} onChange={(e)=> setCarType(e.target.value) } placeholder='Car Type...' />
+            <input type="number" name='rentalDays' value={rentalDays} onChange={(e)=> setRentalDays (e.target.value) }placeholder='renttal days...' />
+            <button  type='submit'> edit</button>
+            
+        </form>
+
+        <Link to="/">Back (Rental List)</Link>
       </form>
     </div>
   );

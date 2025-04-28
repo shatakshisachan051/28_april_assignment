8dae69d41b3e61c36fff0ae76e20d9caf754b1c4
shatakshisachan051 <shatakshisachan1017@gmail.com>
2025-04-28 13:14:59 +0530

    28 april

--- question-2/my-react-app/src/components/AddRental.jsx
+++ question-2/my-react-app/src/components/AddRental.jsx
@@ -1,15 +1,41 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { useDispatch } from 'react-redux';
+import { addRental } from '../store/rentalSlice';
 
 
 function AddRental() {
+    const [customer,setCustomer]= useState('');
+    const [carType,setCarType] = useState('');
+    const [rentalDays, setRentalDays]=useState('');
+    const dispatch =useDispatch();
+
+    function handleSumbit(e){
+        e.preventDefault()
+
+        let rental ={
+            id=Date.now(),
+            customer,
+            carType,
+            rentalDays:parseInt(rentalDays)
+        };
+
+        dispatch(addRental(rental))
+
+        setCustomer("")
+        setCarType("")
+        setRentalDays("")
+
+    }
+
+
   return (
     <div>
         <h1>ADD Rental</h1>
         <form >
-            <input type="text" name="customer" placeholder='Customer Name...' />
-            <input type="text" name="carType" placeholder='Car Type...' />
-            <input type="number" name='rentalDays' placeholder='renttal days...' />
-            <button type='submit'> Add</button>
+            <input type="text" name="customer" onChange={(e)=> setCustomer(e.target.value) } placeholder='Customer Name...' />
+            <input type="text" name="carType" onChange={(e)=> setCarType(e.target.value) } placeholder='Car Type...' />
+            <input type="number" name='rentalDays' onChange={(e)=> setRentalDays (e.target.value) }placeholder='renttal days...' />
+            <button onClick={()=>handleSumbit(e)} type='submit'> Add</button>
             
         </form>
 
